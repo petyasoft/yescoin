2e4ef0d72c5176fd0be539746fd4def7fd36bb86
petyasoft <168456597+petyasoft@users.noreply.github.com>
2024-06-17 22:23:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 from utils.core import create_sessions
 from utils.telegram import Accounts
-from utils.yescoin import Start
+from utils.yescoin import Yescoin
 from data.config import hello,USE_PROXY
 import asyncio
-
+import os
 
 async def main():
     print(hello)
     action = int(input('Выберите действие:\n1. Начать сбор монет\n2. Создать сессию\n>'))
-
+    
+    if not os.path.exists('sessions'):
+        os.mkdir('sessions')
+    
     if action == 2:
         await create_sessions()
 
     if action == 1:
         accounts = await Accounts().get_accounts()
-        with open('proxy.txt','r') as file:
-            proxy = [i.strip() for i in file.readlines()]
+                
         tasks = []
         if USE_PROXY:
+            proxy_dict = {}
+            with open('proxy.txt','r') as file:
+                proxy = [i.strip().split() for i in file.readlines() if len(i.strip().split()) == 2]
+                for prox,name in proxy:
+                    proxy_dict[name] = prox
             for thread, account in enumerate(accounts):
-                if len(proxy) > thread:
-                    tasks.append(asyncio.create_task(Start(account=account, thread=thread, proxy=proxy[thread]).main()))
+                if account in proxy_dict:
+                    tasks.append(asyncio.create_task(Yescoin(account=account, thread=thread, proxy=proxy_dict[account]).main()))
                 else:
-                    tasks.append(asyncio.create_task(Start(account=account, thread=thread,proxy = None).main()))
+                    tasks.append(asyncio.create_task(Yescoin(account=account, thread=thread,proxy = None).main()))
         else:
             for thread, account in enumerate(accounts):
-                tasks.append(asyncio.create_task(Start(account=account, thread=thread,proxy = None).main()))
+                tasks.append(asyncio.create_task(Yescoin(account=account, thread=thread,proxy = None).main()))
         await asyncio.gather(*tasks)
-
-
 
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(main())
-
